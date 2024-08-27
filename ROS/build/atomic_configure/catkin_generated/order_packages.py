48a161547769befc6a5bc229240885731748f774
Omar Eltramsy <113609356+OMRTAREK@users.noreply.github.com>
2024-08-27 20:56:55 +0300

    Add files via upload

---
+++ ROS/build/atomic_configure/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/omar/training_MIA/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/omar/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/omar/catkin_ws/devel;/opt/ros/noetic' != '' else []
